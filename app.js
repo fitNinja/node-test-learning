b68bfa2fc094684fcaa2b988769b74f1b0210ff6
Neelam Rawat <nUjr5xAcQ885rT@>
2022-07-29 15:39:31 +0530

    sett up app for heroku

--- app.js
+++ app.js
@@ -3,48 +3,50 @@ const express = require('express');
 const hbs = require('hbs');
 const app = express();
 
+const port = process.env.PORT || 3000;
+
 app.set('view engine', 'hbs');
-app.set('views', path.join(  __dirname, "/templates/views" ))
-app.use( express.static( path.join(  __dirname, "/public" )) );
+app.set('views', path.join(__dirname, "/templates/views"))
+app.use(express.static(path.join(__dirname, "/public")));
 
-hbs.registerPartials( path.join(  __dirname, "/templates/partials" ));
+hbs.registerPartials(path.join(__dirname, "/templates/partials"));
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render('index', {
         title: "index",
         content: "something",
         name: "Neelam"
     });
 });
-app.get("/help", (req, res)=>{
-    res.render('help',{
+app.get("/help", (req, res) => {
+    res.render('help', {
         title: "help",
         content: "help me",
         name: "Neelam"
     });
 });
-app.get("/about", (req, res)=>{
+app.get("/about", (req, res) => {
     res.render('about', {
         title: "about",
         content: "talk about me",
         name: "Neelam"
     });
 });
-app.get("/weather", (req, res)=>{
+app.get("/weather", (req, res) => {
     res.send({
         forecast: "forecast",
         location: "location",
         name: "Neelam"
     });
 });
-app.get('/help/*', (req, res)=>{
+app.get('/help/*', (req, res) => {
     res.render('help404', {
         title: "404 Error",
         content: "Help article not found",
         name: "Neelam"
     });
 });
-app.get('/*', (req, res)=>{
+app.get('/*', (req, res) => {
     res.render('page404', {
         title: "404",
         content: "Page not found",
@@ -52,6 +54,6 @@ app.get('/*', (req, res)=>{
     });
 });
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000.");
+app.listen(port, () => {
+    console.log("Server is up on port " + port + ".");
 });

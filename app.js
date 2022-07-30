eeb4426206305278abfe139c97add3964e88b761
Neelam Rawat <nUjr5xAcQ885rT@>
2022-07-30 14:11:54 +0530

    changed file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs');
 const app = express();
 
 const port = process.env.PORT || 3000;
-
+console.log("process.env.PORT", process.env.PORT)
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, "/templates/views"))
 app.use(express.static(path.join(__dirname, "/public")));
